6ff53b0df64b1e1554af070b67b682cb6afaed08
Israel Díaz <israel.diaz@alumnos.uach.cl>
2023-07-12 22:46:45 -0400

    estructura base de datos

---
+++ src/sql/hbv.sql
@@ -0,0 +1,84 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : test hbv
+ Source Server Type    : MariaDB
+ Source Server Version : 100614
+ Source Host           : localhost:3306
+ Source Schema         : hbv
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100614
+ File Encoding         : 65001
+
+ Date: 12/07/2023 22:45:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for doctrine_migration_versions
+-- ----------------------------
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE `doctrine_migration_versions`  (
+  `version` varchar(191) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime(0) NULL DEFAULT NULL,
+  `execution_time` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`version`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of doctrine_migration_versions
+-- ----------------------------
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20230713012941', '2023-07-13 01:30:03', 166);
+
+-- ----------------------------
+-- Table structure for messenger_messages
+-- ----------------------------
+DROP TABLE IF EXISTS `messenger_messages`;
+CREATE TABLE `messenger_messages`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime(0) NOT NULL,
+  `available_at` datetime(0) NOT NULL,
+  `delivered_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `IDX_75EA56E0FB7336F0`(`queue_name`) USING BTREE,
+  INDEX `IDX_75EA56E0E3BD61CE`(`available_at`) USING BTREE,
+  INDEX `IDX_75EA56E016BA31DB`(`delivered_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of messenger_messages
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for patient
+-- ----------------------------
+DROP TABLE IF EXISTS `patient`;
+CREATE TABLE `patient`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido_paterno` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido_materno` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `rut` int(11) NOT NULL,
+  `dv` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_nacimiento` date NULL DEFAULT NULL,
+  `sexo` int(11) NULL DEFAULT NULL,
+  `nombre_social` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estado_civil` int(11) NULL DEFAULT NULL,
+  `pais` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `region` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `comuna` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `es_urbano` tinyint(1) NULL DEFAULT NULL,
+  `foto` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
